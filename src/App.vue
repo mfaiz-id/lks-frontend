c9f2a0f632b3e4b30a20238301eca63a1a076178
mfaiz-id <faizmuhammad810@gmail.com>
2021-12-31 21:13:04 +0700

    Install Vue Cli & Bv & Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="margin-top:0px">
+    <Navbar></Navbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from './components/Navbar.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
